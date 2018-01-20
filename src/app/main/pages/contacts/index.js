48befd206d8b0b2240feb4def844348edf1d4079
Rinword <ilyabelenko@mail.ru>
2018-01-20 17:07:58 +0300

    Contacts 5%

--- src/app/main/pages/contacts/index.js
+++ src/app/main/pages/contacts/index.js
@@ -1,20 +1,32 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
-import { InfoPanel } from '../../../uxComponent/UxBox';
-import vk from '../../../../static/icons/social/vk.svg';
-import youtube from '../../../../static/icons/social/youtube.svg';
 import cx from 'classnames';
+
+import { Row, Column } from '../../../uxComponent/UxBox';
+
+import { InfoPanel } from '../../../uxComponent/UxBox';
+
+import youtube from '../../../../static/icons/social/youtube.svg';
 
 import './style.css';
 
 class Contacts extends React.PureComponent {
     render() {
         return (
-            <div id={this.props.id} className={cx('life-style', { slide_active: this.props.isActive })}>
-                <div className="life-style__social">
-                    <InfoPanel title="Группа ВК" subTitle="перейти >" img={vk} />
-                    <InfoPanel title="Канал Youtube" subTitle="перейти >" img={youtube} />
-                </div>
+            <div id={this.props.id} className={cx('contacts', { slide_active: this.props.isActive })}>
+                <div className="contacts__title">Контакты</div>
+                <Row className="contacts__content">
+                    <Row className="contacts__back-form">
+                        <div>form</div>
+                    </Row>
+                    <Column className="contacts__contacts" jc="space-between">
+                        <Column classNme="contacts__company-data">phone, adress</Column>
+                        <Row classNme="contacts__social-data">
+                            <InfoPanel img={youtube} title="Канал Youtube" subTitle="перейти >" />
+                        </Row>
+                    </Column>
+                </Row>
+                <div className="contacts__footer">footer</div>
             </div>
         );
     }
