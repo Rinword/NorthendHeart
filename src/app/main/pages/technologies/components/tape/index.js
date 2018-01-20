394eaf179ddbe806da63f79fc7bf4665b0e0ab98
Rinword <ilyabelenko@mail.ru>
2018-01-20 18:24:43 +0300

    Real tech

--- src/app/main/pages/technologies/components/tape/index.js
+++ src/app/main/pages/technologies/components/tape/index.js
@@ -5,17 +5,30 @@ import PropTypes from 'prop-types';
 import './styles.css';
 
 class TabPanelC extends React.PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideDesc: false,
+        };
+
+        this.onClick = () => {
+            this.setState({ hideDesc: !this.state.hideDesc });
+        };
+    }
+
     render() {
-        const { title, description, img } = this.props;
+        const { title, description, img, width } = this.props;
         return (
-            <div className={cx('tape')}>
+            <div className={cx('tape')} style={{ flex: `0 0 ${width}%` }} onClick={this.onClick}>
                 <div className={cx('tape__img')}>
                     <img src={img} alt="" />
                 </div>
-                <div className={cx('tape__legend')}>
-                    <div className={cx('tape__title')}>{title}</div>
-                    <div className={cx('tape__desc')}>{description}</div>
-                </div>
+                <div className={cx('tape__title')}>{title}</div>
+                <div
+                    className={cx('tape__desc', { tape__desc_hidden: this.state.hideDesc })}
+                    dangerouslySetInnerHTML={{ __html: description }}
+                />
             </div>
         );
     }
@@ -27,6 +40,7 @@ TabPanelC.propTypes = {
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
+    width: PropTypes.number.isRequired,
     getRef: PropTypes.func,
 };
 
--- src/app/main/pages/technologies/index.js
+++ src/app/main/pages/technologies/index.js
@@ -11,7 +11,7 @@ class Technologies extends React.PureComponent {
     render() {
         return (
             <div id={this.props.id} className={cx('tech', { slide_active: this.props.isActive })}>
-                {tapes.map(tape => <Tape key={tape.img} {...tape} />)}
+                {tapes.map(tape => <Tape key={tape.img} {...tape} width={100 / tapes.length} />)}
             </div>
         );
     }
