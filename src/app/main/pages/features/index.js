3566a346c9acefa8206e642417e67cec7cc73c6d
Rinword <ilyabelenko@mail.ru>
2018-01-28 18:21:13 +0300

    Features, Projects, Tech desktop responsive

--- src/app/main/pages/features/index.js
+++ src/app/main/pages/features/index.js
@@ -6,36 +6,42 @@ import ReactIntense from '../../../../libs/react-intense/ReactIntense';
 
 import features from '../../../content/features';
 
+import Responsive from 'react-responsive';
 import './style.css';
+
+const Desktop = props => <Responsive {...props} minWidth={798} />;
+const Mobile = props => <Responsive {...props} maxWidth={797} />;
 
 class Features extends React.PureComponent {
     render() {
         return (
-            <div id={this.props.id} className={cx('features', { slide_active: this.props.isActive })}>
-                <div className="features__background" />
-                <div className="features__title">{this.props.title}</div>
-                <Row cls="features__content" jc="space-around" width="auto" ai="center" padding="0 50px">
-                    {features.map(feature => (
-                        <Column key={feature.title} height="auto" ai="center" cls="features__show-panel show-panel">
-                            <div className="show-panel__img-wrap">
-                                <ReactIntense
-                                    caption={feature.desc}
-                                    title={feature.title}
-                                    className="show-panel__img img_feature"
-                                    src={feature.img}
-                                    alt=""
-                                    thumbnailSrc={feature.img}
-                                    vertical={true}
-                                />
-                            </div>
-                            <div className="show-panel__legend">
-                                <div className="show-panel__title">{feature.title}</div>
-                                <div className="show-panel__desc">{feature.desc}</div>
-                            </div>
-                        </Column>
-                    ))}
-                </Row>
-            </div>
+            <Desktop>
+                <div id={this.props.id} className={cx('features', { slide_active: this.props.isActive })}>
+                    <div className="features__background" />
+                    <div className="features__title">{this.props.title}</div>
+                    <Row cls="features__content" jc="space-around" width="auto" ai="center" padding="0 50px">
+                        {features.map(feature => (
+                            <Column key={feature.title} height="auto" ai="center" cls="features__show-panel show-panel">
+                                <div className="show-panel__img-wrap">
+                                    <ReactIntense
+                                        caption={feature.desc}
+                                        title={feature.title}
+                                        className="show-panel__img img_feature"
+                                        src={feature.img}
+                                        alt=""
+                                        thumbnailSrc={feature.img}
+                                        vertical={true}
+                                    />
+                                </div>
+                                <div className="show-panel__legend">
+                                    <div className="show-panel__title">{feature.title}</div>
+                                    <div className="show-panel__desc">{feature.desc}</div>
+                                </div>
+                            </Column>
+                        ))}
+                    </Row>
+                </div>
+            </Desktop>
         );
     }
 }
