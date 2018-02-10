739896b70dd929461732586c41c611ab318a09e3
Rinword <ilyabelenko@mail.ru>
2018-02-10 17:35:29 +0300

    Features refactor + mobile

--- src/app/content/features.js
+++ src/app/content/features.js
@@ -1,15 +1,19 @@
 import feature1 from '../../static/img/feature1.jpg';
 import feature2 from '../../static/img/feature2.jpg';
 
-export default [
-    {
-        title: 'Модульная архитектура',
-        desc: 'Всегда можно расширить полезную площадь',
-        img: feature1,
-    },
-    {
-        title: 'Конечное число форм —',
-        desc: 'бесконечное число формобразований',
-        img: feature2,
-    },
-]
+export default {
+    features: [
+        {
+            title: 'Модульная архитектура',
+            desc: 'Всегда можно расширить полезную площадь',
+            img: feature1,
+        },
+        {
+            title: 'Конечное число форм —',
+            desc: 'бесконечное число формобразований',
+            img: feature2,
+        },
+    ],
+    title: 'Мы придерживаемся инетресных концепций',
+    desc: 'Поглядите какие штуки мы проворачиваем',
+}
