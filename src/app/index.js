a686f602144886c44803b257264626a9bed40e6c
Rinword <ilyabelenko@mail.ru>
2018-03-17 16:50:06 +0300

    Sharing social

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import Modal from 'react-modal';
+import cx from 'classnames';
 
 import { id } from '../gaTokens';
 import ReactGA from 'react-ga';
+
+import {
+    FacebookShareButton,
+    WhatsappShareButton,
+    TelegramShareButton,
+    EmailShareButton,
+    FacebookIcon,
+    WhatsappIcon,
+    TelegramIcon,
+    EmailIcon,
+} from 'react-share';
 
 import { Row } from './uxComponent/UxBox';
 import Main from './main';
@@ -22,6 +34,7 @@ class App extends Component {
 
         this.state = {
             activePage: 'slide1',
+            showShare: true,
         };
 
         this.onMenuSelect = item => {
@@ -29,6 +42,10 @@ class App extends Component {
         };
         this.onMouseSelect = item => {
             this.setState({ activePage: item });
+        };
+
+        this.toggleShare = () => {
+            this.setState({ showShare: !this.state.showShare });
         };
 
         validations();
@@ -40,12 +57,42 @@ class App extends Component {
     }
 
     render() {
+        const shareUrl = 'https://plusmodul.com';
+        const quote = 'PLUSMODUL';
         return (
             <div className="app" id="app">
                 <HeaderMenu activePage={this.state.activePage} onSelectItem={this.onMenuSelect} cls="app__header" />
                 <Row cls="app__body">
                     <Main activePage={this.state.activePage} onSelectPage={this.onMouseSelect} />
                 </Row>
+                <div className="app__share-area">
+                    <Row
+                        cls={cx('app__share-container', { 'app__share-container_opened': this.state.showShare })}
+                        jc="space-around"
+                        ai="center"
+                        width="auto"
+                        height="auto"
+                    >
+                        <FacebookShareButton url={shareUrl} quote={quote}>
+                            <FacebookIcon size={32} round />
+                        </FacebookShareButton>
+                        <WhatsappShareButton url={shareUrl} quote={quote}>
+                            <WhatsappIcon size={32} round />
+                        </WhatsappShareButton>
+                        <TelegramShareButton url={shareUrl} quote={quote}>
+                            <TelegramIcon size={32} round />
+                        </TelegramShareButton>
+                        <EmailShareButton url={shareUrl} quote={quote}>
+                            <EmailIcon size={32} round />
+                        </EmailShareButton>
+                    </Row>
+                    <div
+                        onClick={this.toggleShare}
+                        className={cx('app__share-hider', { 'app__share-hider_opened': this.state.showShare })}
+                    >
+                        <p>{this.state.showShare ? '-' : '+'}</p>
+                    </div>
+                </div>
             </div>
         );
     }
